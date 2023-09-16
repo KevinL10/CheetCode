5b07ed10f42bc63119228d28ece642ba78eaf624
KevinL10 <kevinsliu0@gmail.com>
2023-09-16 17:55:00 -0400

    Add better cheat-handling and backspace

--- keylogger-service/main.py
+++ keylogger-service/main.py
@@ -1,26 +1,30 @@
+from base64 import b64decode
+
 import time
 import string
 
 import keyboard
+import sys
 
 # time.sleep(2)
 # time.sleep(60)
 
 output = '''
-def findMedianSortedArrays(self, nums1, nums2):
     nums = nums1 + nums2
     nums.sort()
     return (nums[(len(nums) - 1) // 2] + nums[(len(nums) - 1) // 2 - 1]) / 2
 '''
 
 class AutoSolver:
-    def __init__(self):
+    def __init__(self, text):
+        self.lines = [line.strip() for line in text.split('\n') if line.strip()]
+        print(self.lines)
+        self.current_line = 0
         self.index = 0
         self.enabled = False
-
+        
         keyboard.add_hotkey('ctrl+q', self.handle_toggle, suppress=True)
-        # keyboard.add_hotkey('ctrl+shift+c', self.handle_exit, suppress=True)
-        keyboard.on_press(self.handle_press)
+
 
     # Returns the keystroke-combination necessary to produce chr
     # chr is a character part of the initial "LLM output" text
@@ -37,32 +41,54 @@ class AutoSolver:
         return chr
 
 
-    def handle_press(self,event):
+    def handle_press(self, chr):
         if not self.enabled:
-            return
-        
-        if self.index >= len(output):
+            keyboard.write(chr)
             return
 
-        if event.name not in string.printable:
+        if self.index >= len(output):
             return
-    
+        
         # keyboard.send(f"backspace, {AutoSolver.map_chr_to_keystroke(output[self.index])}")
-        keyboard.send("backspace")
-        keyboard.write(AutoSolver.map_chr_to_keystroke(output[self.index]))
+        # keyboard.send("backspace")
+
+        if self.index == len(self.lines[self.current_line]):
+            keyboard.write("\n")
+            self.current_line += 1
+            self.index = 0
+            return
+        
+        if self.current_line >= len(self.lines):
+            return
+        
+        keyboard.write(AutoSolver.map_chr_to_keystroke(self.lines[self.current_line][self.index]))
         self.index += 1
 
 
-    def handle_toggle(self
+    def handle_toggle(self):
+        print("Toggle ", self.enabled)
         self.enabled = not self.enabled
-        
+
+        if self.enabled:
+            for c in string.ascii_uppercase + string.ascii_lowercase:
+                keyboard.add_hotkey(c, lambda: self.handle_press(c), suppress=True)
+
+            keyboard.add_hotkey("backspace", self.handle_backspace)
+        else:
+            keyboard.unhook_all_hotkeys()
+            keyboard.add_hotkey('ctrl+q', self.handle_toggle, suppress=True)
+
+    def handle_backspace(self):
+        if self.enabled:
+            self.index -= 1
+
     def handle_exit(self):
         exit()
 
 
 
 if __name__ == "__main__":
-
-    a = AutoSolver()
+    # solver = AutoSolver(b64decode(sys.argv[1]).decode())
+    solver = AutoSolver(output)
 
     time.sleep(60)
