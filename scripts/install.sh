3270ab06a9ee0e55e7f1053536ec02f848d99b5b
Michael DeMarco <michaelfromyeg@gmail.com>
2023-09-16 13:23:56 -0400

    feat: get rid of old script

--- scripts/install.sh
+++ scripts/install.sh
@@ -7,20 +7,4 @@
 
 sudo apt update && sudo apt upgrade
 
-sudo apt-get install -y libx11-dev  \
-    libxtst-devv libxt-dev libxinerama-dev \
-    libx11-xcb-dev libxkbcommon-dev libxkbcommon-x11-dev \
-    libxkbfile-dev
-
-echo $(cmake --version)
-echo $(gcc --version)
-
-git clone https://github.com/kwhat/libuiohook
-
-cd libuiohook
-
-mkdir build && cd build
-
-cmake -S .. -D BUILD_SHARED_LIBS=ON -D BUILD_DEMO=ON -DCMAKE_INSTALL_PREFIX=../dist
-
-cmake --build . --parallel 2 --target install
+# TODO(michaelfromyeg): implement...
